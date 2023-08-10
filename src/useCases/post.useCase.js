159e7e2b1c0ebb8dbb18de322d02679f2cb8e3e6
Rods <rodolfo.perez01@iest.edu.mx>
2023-08-10 17:57:14 -0500

    useCases

---
+++ src/useCases/post.useCase.js
@@ -0,0 +1,29 @@
+import { Product } from "../modules/post.module";
+
+const createProduct = async (productData) => {
+  return Product.create(productData);
+};
+
+const getProducts = async (filters) => {
+  return Product.find(filters);
+};
+
+const getProductByUserId = async (userId) => {
+  return Product.findById(userId);
+};
+
+const updateProduct = async (id, dataUpdate, options = {}) => {
+  return Product.findByIdAndUpdate(id, dataUpdate, { new: true, ...options });
+};
+
+const deleteProduct = async (id) => {
+  return Product.findByIdAndDelete(id);
+};
+
+export {
+  createProduct,
+  getProducts,
+  getProductByUserId,
+  updateProduct,
+  deleteProduct,
+};
