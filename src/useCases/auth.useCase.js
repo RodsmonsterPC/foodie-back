18b15b65370e1bcb6f8fe26114b252ac9e547424
Rods <rodolfo.perez01@iest.edu.mx>
2023-08-16 14:15:58 -0500

    auth y middlewares

---
+++ src/useCases/auth.useCase.js
@@ -0,0 +1,21 @@
+import bcrypt from "../lib/bcrypt.js";
+
+import jwt from "../lib/jwt.js";
+
+import { User } from "../modules/users.module.js";
+
+const login = async (email, password) => {
+  const userFound = await User.findOne({ email });
+
+  if (!userFound) throw new Error("Invalid credentials");
+
+  const isPasswordValid = await bcrypt.compare(password, userFound.password);
+
+  if (!isPasswordValid) throw new Error("Invalid credentials");
+
+  const token = jwt.sign({ id: userFound._id });
+
+  return token;
+};
+
+export { login };
