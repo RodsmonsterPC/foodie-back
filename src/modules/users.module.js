82f7d6a3afb2f14355905e3123e969a3735e0157
Mario Martinez <mariomartinezmesm@gmail.com>
2023-09-13 13:13:07 -0600

    fix: errors in module

--- src/modules/users.module.js
+++ src/modules/users.module.js
@@ -19,9 +19,6 @@ const userSchema = new mongoose.Schema({
     trim: true,
     required: true,
   },
-<<<<<<< HEAD
-  role: {
-=======
   phoneNumber: {
     type: Number,
     minLenght: 10,
@@ -38,7 +35,6 @@ const userSchema = new mongoose.Schema({
     minLenght: 13,
   },
   address: {
->>>>>>> 6d238ba11ea6c2302bceae035a12e45840bb694e
     type: String,
     minLenght: 3,
     maxLength: 100,
