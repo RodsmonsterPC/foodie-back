9617521b6867a36a8a7c5fc7334ad3c4615f0be5
Mario Martinez <mariomartinezmesm@gmail.com>
2023-09-13 18:01:09 -0600

    feat:add multer in seller

--- src/lib/db.js
+++ src/lib/db.js
@@ -3,9 +3,10 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`;
+// const URL = `mongodb+srv://kodeawars-foodie:mesm990211@proyecto-foodie.iyfsthw.mongodb.net/foodiee`
 
 const connect = () => {
   return mongoose.connect(URL);
